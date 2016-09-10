5a43ede568356a8a7b7f349d7daf5b3d97461b74
Peter Reimer <peter@4pi.org>
2016-09-10 13:41:25 +0200

    loading json from javascript

--- static/js/panorama.js
+++ static/js/panorama.js
@@ -1,31 +1,6 @@
-pannellum.viewer('panorama', 
-	{
-		"default": {
-			"autoLoad": true, 
-			"firstScene": "elbtunnel", 
-			"hotSpotDebug": true
-		}, 
-		"scenes": {
-			"elbtunnel": {
-				"autoRotate": 1, 
-				"compass": true, 
-				"hfov": 70, 
-				"hotSpots": [], 
-				"multiRes": {
-					"basePath": "http://ultra02/~reimer/tiles/elbtunnel", 
-					"cubeResolution": 4224, 
-					"extension": "jpg", 
-					"fallbackPath": "/fallback/%s", 
-					"maxLevel": 4, 
-					"path": "/%l/%s%y_%x", 
-					"tileResolution": 528
-				}, 
-				"northOffset": 240.8150808, 
-				"pitch": 10, 
-				"title": "Alter Elbtunnel", 
-				"type": "multires", 
-				"yaw": 292
-			}
-		}
-	}
-);
+var tour_data = "//" + location.host + location.pathname + "tour.json"
+
+$.getJSON(tour_data, function(data) {
+    pannellum.viewer('panorama', data);
+    console.log(data);
+});
