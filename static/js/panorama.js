7006bb9291b98507bb9bafc60c3fdb9f66624c59
Peter Reimer <peter@4pi.org>
2016-09-11 23:56:06 +0200

    logging vfov

--- static/js/panorama.js
+++ static/js/panorama.js
@@ -1,6 +1,6 @@
 var tour_data = "//" + location.host + location.pathname + "tour.json"
 
 $.getJSON(tour_data, function(data) {
-    pannellum.viewer('panorama', data);
-    console.log(data);
+    pv = pannellum.viewer('panorama', data);
+    console.log(pv.getHfov());
 });
